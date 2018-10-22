f532eecb3e0161cbc2a6cf3031b78130c3fc1f6a
jason-sa <jandlsalazeradams@gmail.com>
2018-10-22 16:59:42 -0700

    init

---
+++ py_features/data_transformation.py
@@ -0,0 +1,135 @@
+''' Transformation of raw web traffic data from retail rocket into addtocart observations
+
+Author: Jason Salazer-Adams
+Date: 10/22/2018
+
+'''
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+pd.set_option('mode.chained_assignment',None)
+
+DATE_FILTER = datetime(2015, 8, 15)
+EVENTS_FILE = '../data/events.csv'
+SESSION_TIME_LIMIT = 600 ## Should consider building a time limit per user, instead of global
+
+def convert_to_local(x):
+    ''' Helper function to convert timestamp in milli-seconds to a date time
+    '''
+    return datetime.fromtimestamp(x/1000)
+
+def calc_session_id(df, mask):
+    ''' Calculates a web session id based on a data frame and a session filter logic
+
+    df: data frame of events
+    mask: filter on the data frame which defines a session
+
+    return: pd.DataFrame (same as df with session_id added)
+    '''
+
+    df['session_id'] = np.nan
+    ind = df.groupby('visitorid').head(1).index
+    df.loc[ind, 'session_id'] = 1
+
+    count_session = df[mask].shape[0] + 1
+    df.loc[mask, 'session_id'] = np.arange(2,count_session+1)
+
+    # fill in all of the gaps
+    df.session_id.fillna(method = 'ffill', inplace=True)
+
+    # make the session id unique
+    df.session_id = df.visitorid.astype(str) + '_' + df.session_id.astype(int).astype(str)
+    
+    return df
+
+# get data
+events = pd.read_csv(EVENTS_FILE)
+events['local_date_time'] = events.timestamp.apply(convert_to_local)
+print(f'Total number of events: {events.shape[0]:,}')
+print()
+
+# reduce data set size for MVP
+events_trimmed = events[events.local_date_time >= DATE_FILTER]
+
+print(f'Total number of trimmed events: {events_trimmed.shape[0]:,}')
+print()
+
+# first calcualte sessions for each buy transaction
+events_trimmed.sort_values(['visitorid','local_date_time'], inplace=True)
+events_trimmed['prev_event'] = events_trimmed.groupby('visitorid').event.shift(1)
+sub = (events_trimmed.event == 'view') & (events_trimmed.prev_event == 'transaction')
+events_trimmed = calc_session_id(events_trimmed, sub)
+
+print(f'Total number of sessions dividing on transaction: {len(events_trimmed.session_id.unique()):,}')
+print()
+
+# calcualte the time diff within each session
+events_trimmed.sort_values(['session_id', 'local_date_time'], inplace = True)
+
+# grouby + diff is slow may need to fix
+events_trimmed['time_diff'] = (events_trimmed
+                               .groupby('session_id')['timestamp']
+                               .diff(1)
+                               .fillna(0) 
+                               / 1000)
+
+events_trimmed['page_length'] = events_trimmed.groupby('visitorid').time_diff.shift(-1)
+
+# re-calaculate sessions with a new session starting whenever a buy occurs or if a view lasts longer than 3.5 minutes
+sub = (((events_trimmed.event == 'view') 
+       & (events_trimmed.prev_event == 'transaction'))
+      | ((events_trimmed.event.isin(['view','addtocart']) )
+        & (events_trimmed.time_diff > SESSION_TIME_LIMIT)))
+
+events_trimmed = calc_session_id(events_trimmed, sub)
+
+print(f'Total number of sessions adding a time limit: {len(events_trimmed.session_id.unique()):,}')
+print()
+
+# trim down to addtocart events only
+events_addtocart = events_trimmed[events_trimmed.event == 'addtocart']
+
+order_history = events_addtocart.loc[:,['session_id', 'visitorid','itemid', 'local_date_time']]
+
+# visitors with multiple sessions only '_1' is alwasy the first session for each visitor
+visitors = order_history[~order_history.session_id.str.endswith('_1')].visitorid.unique()
+order_history = order_history[order_history.visitorid.isin(visitors)]
+order_history = order_history.groupby(['session_id','visitorid','itemid'])['local_date_time'].max().reset_index()
+
+unique_visitor_item = order_history.loc[:,['visitorid', 'itemid']].drop_duplicates()
+total_order_history = unique_visitor_item.merge(order_history, how='outer', on='visitorid')
+
+# Calaculate what was in the cart over time based on what the visitor has ever purchased
+total_order_history['in_cart'] = 0
+
+mask = (total_order_history.itemid_y == total_order_history.itemid_x)
+total_order_history.loc[mask, 'in_cart'] = 1
+
+# Clean-up columns
+total_order_history.drop(columns='itemid_y', inplace=True)
+total_order_history.rename(columns={'itemid_x':'itemid'}, inplace=True)
+
+# calacualte order sequence
+total_order_history['seq'] = (total_order_history.session_id
+                                        .str.split('_')
+                                        .apply(lambda x: x[1])
+                                        .astype(int))
+
+total_order_history['order_seq'] = (total_order_history
+                                  .groupby('visitorid')['seq']
+                                  .rank(method='dense'))
+
+total_order_history.drop(columns='seq',inplace=True)
+
+# split last_order and all other orders
+last_order = total_order_history.groupby('visitorid')['session_id'].last()
+
+observations = total_order_history[total_order_history.session_id.isin(last_order)]
+prior_observations = total_order_history.drop(observations.index)
+
+# save data
+events_trimmed.to_pickle('../data/events_trimmed.pkl')
+total_order_history.to_pickle('../data/total_order_history.pkl')
+observations.to_pickle('../data/observations.pkl')
+prior_observations.to_pickle('../data/prior_observations.pkl')
