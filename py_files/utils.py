e7daf277e98a49e99039aba06af6b324dab90c66
jason-sa <jandlsalazeradams@gmail.com>
2018-11-01 21:20:00 -0700

    added comments

--- py_files/utils.py
+++ py_files/utils.py
@@ -1,7 +1,11 @@
 import pandas as pd 
 
 def write_to_pickle(df,name):
+        ''' Utility to write data frames to pickles
+        '''
         df.to_pickle(f'../data/{name}.pkl')
 
 def load_pickle(name):
-    return pd.read_pickle(f'../data/{name}.pkl')
+        ''' Utility to load data frames from pickles
+        '''
+        return pd.read_pickle(f'../data/{name}.pkl')
