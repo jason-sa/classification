1b6c4109b908cd83837922ffe0e3bc171c3569ed
jason-sa <jandlsalazeradams@gmail.com>
2018-12-12 14:39:53 -0800

    init

---
+++ classification/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classification of whether or not a visitor will buy next to an e-commerce site',
+    author='Jason Salazer-Adams',
+    license='MIT',
+)
