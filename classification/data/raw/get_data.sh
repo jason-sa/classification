4aafaee8027c2aacac0eec6203ed09a342baf943
jason-sa <jandlsalazeradams@gmail.com>
2019-01-21 11:00:58 -0800

    removed backup

--- classification/data/raw/get_data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Retrieves the raw data from Kaggle
-# Assume kaggle command line tool is installed and configured
-kaggle datasets download -d retailrocket/ecommerce-dataset
-unzip ecommerce-dataset.zip 
-rm ecommerce-dataset.zip
--- classification/docs/README.md
+++
@@ -1,3 +0,0 @@
-# Documents directory
-
-Documents generated from the analysis of the buy or not classification model utilizing the RetailRocket data set.
